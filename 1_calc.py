9ff46c1f754b1385e92ef80557ccfe805d550a49
Shubh0306 <107140092+Shubh0306@users.noreply.github.com>
2024-05-25 17:04:09 +0530

    Add files via upload

---
+++ 1_calc.py
@@ -0,0 +1,18 @@
+print("*******CALCULATOR************")
+print("enter 2 numbers")
+num1 = int(input("enter 1st number:"))
+num2 = int(input("enter 2nd number:"))
+sum= num1 + num2
+sub= num1-num2
+multiply= num1*num2
+division= num1/num2
+exp= num1**num2
+mod= num1%num2
+fd= num1//num2
+print("Sum of 2 numbers are:", sum)
+print("Subtraction of 2 numbers are:", sub)
+print("Multiplication of 2 numbers are:", multiply)
+print("Division of 2 numbers are:", division)
+print("Exponential of 2 numbers are:", exp)
+print("Modulus of 2 numbers are:", mod)
+print("Floor Division of 2 numbers are:", fd)
